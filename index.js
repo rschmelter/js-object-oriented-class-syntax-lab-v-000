c5930dbfc16de826d7735079caa583563077d254
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-15 23:29:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,17 @@ class BoardMember {
       }
       sayHi(){
       return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-          
+
         }
-        
+
       }
+
+class Ceo extends BoardMember {
+
+  hireEmployee() {
+    return 'Welcome aboard!'
+  }
+
+
+  
+}
